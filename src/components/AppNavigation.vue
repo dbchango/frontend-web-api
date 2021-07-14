1274db904049f724d6dfe26bcf85d5fd983adfa0
David Chango <dbchango@espe.edu.ec>
2021-07-14 14:03:24 -0500

    first commit

---
+++ src/components/AppNavigation.vue
@@ -0,0 +1,35 @@
+<template>
+
+    <v-navigation-drawer  app>
+        <v-list>
+            <v-list-item class="px-2">
+                <v-list-item-avatar>
+                    <v-img src="https://randomuser.me/api/portraits/women/85.jpg"></v-img>
+                </v-list-item-avatar>
+                <v-list-item-content>
+                    <v-list-item-title class="text-h6">Username</v-list-item-title>
+                    <v-list-item-subtitle class="text-h7">Email</v-list-item-subtitle>
+                </v-list-item-content>
+            </v-list-item>
+         
+        </v-list>
+        <v-divider></v-divider>
+        <v-list nav dense>
+            <v-list-item link>
+                <v-list-item-icon>
+                    <v-icon>mdi-folder</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title>Files</v-list-item-title>
+            </v-list-item>
+        </v-list>
+    </v-navigation-drawer>
+    
+</template>
+<script>
+export default {
+    name: 'AppNavigation'
+}
+</script>
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,26 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+    <div class="home">
+        <div>
+            <AppNavigation/>
+            
+        </div>
+        <div>
+            <v-main>
+            <img alt="Vue logo" src="../assets/logo.png" />
+            </v-main>
+        </div>
+        
+    </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import AppNavigation from '../components/AppNavigation.vue';
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
-  },
+    name: 'Home',
+    components: {
+        AppNavigation
+    },
 };
 </script>
