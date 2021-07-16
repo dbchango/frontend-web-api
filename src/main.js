8268861c73a069c0684de4ab42653af46d2aebbe
victor131999 <tovir.gu.hu@hotmail.com>
2021-07-16 00:15:21 -0500

    inicio de login y registro

--- src/main.js
+++ src/main.js
@@ -7,9 +7,16 @@ import axios from 'axios';
 import VueAxios from 'vue-axios';
 import Vuelidate from 'vuelidate';
 import '@babel/polyfill';
+import {BootstrapVue,IconsPlugin} from 'bootstrap-vue'
+
 Vue.use(Vuelidate);
 Vue.use(VueAxios, axios);
 Vue.config.productionTip = false;
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 new Vue({
     router,
